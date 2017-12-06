3367fa7479d95a2fb544d58c407acbd8278b70a1
Jon Spencer <jon@jonspencer.ca>
2017-12-05 23:12:46 -0800

    contract the configuration before dumping

--- test/load-test.js
+++ test/load-test.js
@@ -1,6 +1,7 @@
 /* eslint-env node, mocha */
 
 const expect = require("chai").expect;
+const config = require("../lib/config");
 const load = require("../lib/load");
 const fs = require('fs-extra');
 
@@ -42,6 +43,44 @@ const ExpectedConfig = {
 	},
 };
 
+
+const ContractedConfig = {
+	tables: {
+		js: {
+			columns: {
+				id: "id",
+				another: { type: "text", unique: true },
+				bold: "text",
+			},
+			indices: {
+				explicit_name: { columns: [ "another", "bold" ], unique: true }
+			}
+		},
+		json: {
+			columns: {
+				hello: { type: "text", default: "goodbye" },
+				another: "integer"
+			},
+		},
+		yaml: {
+			columns: {
+				id: "id",
+				text: "timestamp with time zone",
+				time: "integer"
+			},
+			indices: {
+				yaml_text_index: { columns: "text" },
+				yaml_id_time_index: { columns: [ "id", "time" ] }
+			}
+		}
+	},
+	types: {
+		foofara: {
+			values: [ "a", "b", "c" ]
+		}
+	}
+};
+
 describe("Loads the configuration file", function() {
 	it("loads the test configuration", function() {
 		return load("test/conf/schema.@(js|json|yaml)", "test/conf/plpgsql/**/*.sql")
@@ -53,6 +92,15 @@ describe("Loads the configuration file", function() {
 				return fs.readFile(`test/conf/plpgsql/${n}.sql`, 'utf8')
 				.then(b => expect(f[n]).to.deep.equal(b));
 			}));
+		});
+	});
+
+	it("contracts the test configuration", function() {
+		return load("test/conf/schema.@(js|json|yaml)", "test/conf/plpgsql/**/*.sql")
+		.then(conf => {
+			delete conf.functions;
+			conf = config.contract(conf);
+			expect(conf).to.deep.equal(ContractedConfig);
 		});
 	});
 });
